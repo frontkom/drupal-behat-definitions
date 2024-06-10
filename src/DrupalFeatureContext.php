8706e4411f4f9aafeeed9297c2d05044f8b38fff
Eirik S. Morland <eirik@morland.no>
2024-06-10 12:41:42 +0200

    Add a step to run an update hook

--- src/DrupalFeatureContext.php
+++ src/DrupalFeatureContext.php
@@ -11,4 +11,18 @@ class DrupalFeatureContext extends RawDrupalContext {
   const LAST_WATCHDOG_TIME = 'frontkom-behat-drupal:last-wd';
   use FailOnWatchDogTrait;
 
+  /**
+   * Step to run a post update hook.
+   *
+   * @Then I run the post_update hook :hook from module :module
+   */
+  public function iRunThePostUpdateHookFromModule($hook, $module) {
+    /** @var \Drupal\Core\Extension\ModuleHandler $module_handler */
+    $module_handler = \Drupal::moduleHandler();
+    $module_handler->loadInclude($module, 'php', "$module.post_update");
+    // It's possible to include an argument for "sandbox".
+    $sandbox = [];
+    $hook($sandbox);
+  }
+
 }
