eedad6cdaed39d5ec9a9b3d092e12480b047f615
Eirik Stanghelle Morland <eirik@morland.no>
2024-06-13 14:56:20 +0200

    Add a step for file in entity (#5)

--- src/DrupalFeatureContext.php
+++ src/DrupalFeatureContext.php
@@ -2,6 +2,7 @@
 
 namespace Frontkom\DrupalBehatDefinitions;
 
+use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 
 /**
@@ -54,6 +55,49 @@ class DrupalFeatureContext extends RawDrupalContext {
       throw new \Exception('No nodes found for ' . $title);
     }
     return reset($nids);
+  }
+
+  /**
+   * Step definition for attaching files to entities.
+   *
+   * @Then /^I attach file "([^"]*)" to "([^"]*)" "([^"]*)" "([^"]*)" in field "([^"]*)"$/
+   */
+  public function iAttachFileToInField($file_name, $entity_type, $content_type, $title, $field) {
+    $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
+    $definition = \Drupal::entityTypeManager()->getDefinition($entity_type);
+    $entities = $storage->loadByProperties([
+      $definition->getKey('label') => $title,
+      $definition->getKey('bundle') => $content_type,
+    ]);
+    if (count($entities) === 0) {
+      throw new \Exception("No $entity_type found with title $title");
+    }
+    if (count($entities) > 1) {
+      throw new \Exception("Multiple $entity_type found with title $title");
+    }
+    $entity = reset($entities);
+    if (!$entity instanceof FieldableEntityInterface) {
+      throw new \Exception("Entity is not a fieldable entity");
+    }
+    $file = $this->createFile($file_name);
+    $entity->set($field, $file);
+    $entity->save();
+  }
+
+  /**
+   * Helper to create a file from the assets dir.
+   */
+  public function createFile($path) {
+    /** @var \Drupal\file\FileRepositoryInterface $file_repo */
+    $file_repo = \Drupal::service('file.repository');
+    if ($this->getMinkParameter('files_path')) {
+      $fullPath = rtrim(realpath($this->getMinkParameter('files_path')), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $path;
+      if (is_file($fullPath)) {
+        $path = $fullPath;
+      }
+    }
+    $contents = file_get_contents($path);
+    return $file_repo->writeData($contents, sprintf('public://%s', basename($path)));
   }
 
 }
