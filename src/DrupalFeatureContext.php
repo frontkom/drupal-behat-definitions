cc649f895bdd555e96f640f3f80d4e3ad0f3d568
Eirik S. Morland <eirik@morland.no>
2024-06-13 14:48:23 +0200

    Add a step for file in entity

--- src/DrupalFeatureContext.php
+++ src/DrupalFeatureContext.php
@@ -56,4 +56,44 @@ class DrupalFeatureContext extends RawDrupalContext {
     return reset($nids);
   }
 
+  /**
+   * Step definition for attaching files to entities.
+   *
+   * @Then /^I attach file "([^"]*)" to "([^"]*)" "([^"]*)" "([^"]*)" in field "([^"]*)"$/
+   */
+  public function iAttachFileToInField($file_name, $entity_type, $content_type, $title, $field) {
+    $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
+    $definition = \Drupal::entityTypeManager()->getDefinition($entity_type);
+    $entities = $storage->loadByProperties([
+      $definition->getKey('label') => $title,
+      $definition->getKey('bundle') => $content_type,
+    ]);
+    if (count($entities) === 0) {
+      throw new \Exception("No $entity_type found with title $title");
+    }
+    if (count($entities) > 1) {
+      throw new \Exception("Multiple $entity_type found with title $title");
+    }
+    $entity = reset($entities);
+    $file = $this->createFile($file_name);
+    $entity->set($field, $file);
+    $entity->save();
+  }
+
+  /**
+   * Helper to create a file from the assets dir.
+   */
+  public function createFile($path) {
+    /** @var \Drupal\file\FileRepositoryInterface $file_repo */
+    $file_repo = \Drupal::service('file.repository');
+    if ($this->getMinkParameter('files_path')) {
+      $fullPath = rtrim(realpath($this->getMinkParameter('files_path')), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $path;
+      if (is_file($fullPath)) {
+        $path = $fullPath;
+      }
+    }
+    $contents = file_get_contents($path);
+    return $file_repo->writeData($contents, sprintf('public://%s', basename($path)));
+  }
+
 }
